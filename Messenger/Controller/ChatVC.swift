926f4f3abe417b6e85a987652347f301f70bbd36
Maruf <maruf1609@gmail.com>
2019-06-23 01:21:31 +0600

    swreveal done

---
+++ Messenger/Controller/ChatVC.swift
@@ -0,0 +1,26 @@
+//
+//  ChatVC.swift
+//  Messenger
+//
+//  Created by Maruf Khan on 23/6/19.
+//  Copyright © 2019 Maruf Khan. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+       
+    }
+    
+
+
+}
