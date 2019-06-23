be5b54900ca4facdfbd52f45331279f650e65b6c
Maruf <maruf1609@gmail.com>
2019-06-23 17:06:05 +0600

    loginVC done

---
+++ Messenger/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Messenger
+//
+//  Created by Maruf Khan on 23/6/19.
+//  Copyright © 2019 Maruf Khan. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
---
+++ Messenger/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Messenger
+//
+//  Created by Maruf Khan on 23/6/19.
+//  Copyright © 2019 Maruf Khan. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
+
